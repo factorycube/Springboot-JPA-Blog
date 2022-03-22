136b12e47b4cead67a21714366346e26d985c6cd
gglee <gwan1020@naver.com>
2022-03-22 21:39:47 +0900

    테이블 생성 및 설정

---
+++ src/main/java/com/gglee/blog/model/Reply.java
@@ -0,0 +1,34 @@
+package com.gglee.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Reply {
+
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+    private int id; // 시퀀스, auto_increment
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
---
+++ src/main/java/com/gglee/blog/test/Member.java
@@ -0,0 +1,13 @@
+package com.gglee.blog.test;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
