938e2531e4261a32e67da8e3f2b91cb28651ece5
gglee <gwan1020@naver.com>
2022-03-23 12:49:38 +0900

    보드 테이블 내용 수정 및 서머리

--- src/main/java/com/gglee/blog/model/Board.java
+++ src/main/java/com/gglee/blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 
 @Data
@@ -31,9 +32,14 @@ public class Board {
     @ColumnDefault("0")
     private int count; // 조회수
 
-    @ManyToOne // Many = Board, One = User
+    @ManyToOne(fetch = FetchType.EAGER) // Many = Board, One = User
     @JoinColumn(name = "userId")
     private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 카럼을 만들지 마세요.
+    private List<Reply> reply;
+    //EAGER : 게시글 상세보기를 불러올때, 바로 보여주는 꼭 필요한 데이터라면 이 전략을 사용.
+    //LAZY :  하지만, 상세보기를 불러올때, 바로 불러오지 않고, 펼쳐보기를 눌러야 댓글을 보여줄때는 레이지 전략을 사용.
 
     @CreationTimestamp
     private Timestamp createDate;
